7f593d664f42fe7e5dce9dfa7da10faabe7c0c7d
Esther Suh <estherjsuh@gmail.com>
2020-03-09 08:42:10 -0700

    Update one_away.py

--- Chapter 1/1.5/one_away.py
+++ Chapter 1/1.5/one_away.py
@@ -1,11 +1,29 @@
 #One Away
 
+#Quick solution
 def one_away(str1, str2):
     #edge case
-    if abs(len(str1) - len(str2)) >= 2:
+    if abs(len(str1) - len(str2)) > 1:
         return False
 
     for i in str1:
         if i in str2:
             str2 = str2.replace(i,"")
     return len(str2) <= 1
+
+#Replace, Insert, Remove
+def one_away(str1, str2):
+    if abs(len(str1) - len(str2)) > 1:
+        return False
+    count = 0
+    elif len(str1) == len(str2):
+        for letter1, letter2 in zip(str1,str2):
+            if letter1 != letter2:
+                count += 1
+        if count > 1:
+            return False
+        return True
+           
+    else: #fill this in
+        
+        
